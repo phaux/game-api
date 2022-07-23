92567f169293c6ff6b03a18f632fb90788c23dd1
Karol Marcjan <karol.marcjan@gmail.com>
2022-07-24 01:41:56 +0200

    matchmaker: Add preliminary Earthfile

--- matchmaker/generate.go
+++ matchmaker/generate.go
@@ -4,16 +4,9 @@ package generate
 
 //go:generate mkdir -p .bin
 
-//go:generate env GOBIN=${PWD}/.bin go install github.com/bufbuild/buf/cmd/buf
-//go:generate env GOBIN=${PWD}/.bin go install google.golang.org/protobuf/cmd/protoc-gen-go
-//go:generate env GOBIN=${PWD}/.bin go install github.com/bufbuild/connect-go/cmd/protoc-gen-connect-go
+//go:generate env GOBIN=${PWD}${CI_GOBIN_PREFIX}/.bin go install github.com/bufbuild/buf/cmd/buf
+//go:generate env GOBIN=${PWD}${CI_GOBIN_PREFIX}/.bin go install google.golang.org/protobuf/cmd/protoc-gen-go
+//go:generate env GOBIN=${PWD}${CI_GOBIN_PREFIX}/.bin go install github.com/bufbuild/connect-go/cmd/protoc-gen-connect-go
 
-//go:generate echo "Installed tools"
-
-//go:generate env PATH=${PWD}/.bin:$PATH buf lint
-
-//go:generate echo "Verified protobuf definitions"
-
-//go:generate env PATH=${PWD}/.bin:$PATH buf generate rpc/v1/api.proto -v
-
-//go:generate echo "Generated protobuf code"
+//go:generate env PATH=${PWD}${CI_GOBIN_PREFIX}/.bin:$PATH buf lint
+//go:generate env PATH=${PWD}${CI_GOBIN_PREFIX}/.bin:$PATH buf generate rpc/v1/api.proto -v
